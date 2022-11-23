7d428614bc4e6176c7ba227254ac3a693bf0eaef
ItaloLis <87744963+ItaloLis@users.noreply.github.com>
2022-11-23 08:16:41 -0300

    Update domino.py

--- domino.py
+++ domino.py
@@ -1,5 +1,9 @@
-n = int(input())
-if 0 <= n <= 10000:
-    print(int(((n + 1)*(n + 2))/2))
-else:
-    print("n deve estar entre 0 e 10.000")
+while True:
+    try:
+        n = int(input())
+        if 0 <= n <= 10000:
+             print(int(((n + 1)*(n + 2))/2))
+        break
+    except ValueError:
+        print("n deve estar entre 0 e 10.000! \n Tente  novamente!")
+
