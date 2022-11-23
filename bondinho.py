af4e47295b5d0e71895421c9d9bfd384255e05c5
ItaloLis <87744963+ItaloLis@users.noreply.github.com>
2022-11-23 09:55:59 -0300

    Update bondinho.py

--- bondinho.py
+++ bondinho.py
@@ -25,3 +25,18 @@ while True:
         else:
             print("Capacidade máxima = 50 \n Tente  novamente!")
         break
+
+
+#ou
+
+
+a = int(input())
+m = int(input())
+
+if a <= 50 and m <= 50:
+        if a + m <= 50:
+                print("S")
+        else:
+                print("N")
+else:
+        print("Capacidade máxima = 50 \n Tente  novamente!")
