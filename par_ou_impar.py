5d7516f5ddc0003748b8862565eaf4925458d794
ItaloLis <87744963+ItaloLis@users.noreply.github.com>
2022-11-23 08:47:39 -0300

    Update par_ou_impar.py

--- par_ou_impar.py
+++ par_ou_impar.py
@@ -3,13 +3,7 @@ d1 = int(input())
 d2 = int(input())
 soma = d1 + d2
 
-if p == 0:
-    if soma%2 == 0:
-        print(0)
-    else:
-        print(1)
-elif p == 1:
-    if soma%2 == 0:
-        print(1)
-    else:
-        print(0)
+if p == 0 and soma%2 == 0:
+    print(0)
+elif p == 1 and soma%2! == 0:
+    print(0)
