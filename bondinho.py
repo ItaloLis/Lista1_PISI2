f4c4370ea72782348df513a554dcd1e7248b7cab
ItaloLis <87744963+ItaloLis@users.noreply.github.com>
2022-11-23 08:27:41 -0300

    Update bondinho.py

--- bondinho.py
+++ bondinho.py
@@ -1,10 +1,12 @@
-a = int(input())
-m = int(input())
-
-if a <= 50 and m <= 50:
-    if a + m <= 50:
-        print("S")
-    else:
-        print("N")
-else:
-    print("Capacidade máxima = 50 pessoas")
+while True:
+    try:
+        a = int(input())
+        m = int(input())
+        if a <= 50 and m <= 50:
+                if a + m <= 50:
+                        print("S")
+                else:
+                        print("N")
+        break
+    except ValueError:
+        print("Capacidade máxima = 50 \n Tente  novamente!")
